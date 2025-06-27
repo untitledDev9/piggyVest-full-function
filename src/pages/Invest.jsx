ae576fa1b236a7b83e22090974f10be642d5d90b
untitled <ahmedsuleiman940@gmail.com>
2025-06-28 00:30:29 +0100

    done with invest hero

--- src/pages/Invest.jsx
+++ src/pages/Invest.jsx
@@ -4,10 +4,15 @@ import PiggyLogo from "../assets/logo.svg";
 import Save from '../pages/Save';
 import Resources from "../pages/Resources";
 import ButtonCard from '../reuseable/ButtonCard';
+import { FaApple } from "react-icons/fa";
+import InvestHero from "../assets/investHeroImage.png";
+import { BiLogoPlayStore } from "react-icons/bi";
+import investBg from "../assets/investbg.png";
 
 const Invest = () => {
   return (
-    <div className=' bg-[#7913E5]'>
+    <>
+    <div className='relative bg-[#7913E5]'>
       <Header
         logo={PiggyLogo}
         linkColor='#fff'
@@ -28,29 +33,50 @@ const Invest = () => {
         />}
       />
 
-      <div>
-        <div>
-          <h1>Invest on the go</h1>
-          <button> Up to 35% returns</button>
-          <p>Invest securely and confidently on the go. Up to 35% returns, 6-12 month duration.</p>
-          <div>
-            <ButtonCard 
-            icon=''
-            title="Get on iPhone"
-            textColor='#fff'
-            bgColor='#122231'
+      <div className=' relative  max-w-[1280px] px-16 mx-auto flex pt-28 justify-between items-center'>
+        <div className=' flex flex-col gap-5'>
+          <h1 className=' text-[48px] text-white font-[700]'>Invest on the go</h1>
+          <button className=' mb-5 w-fit text-[17px] text-[#7913e5] font-[400] bg-white py-2.5 px-4 rounded-[17px]'> Up to 35% returns</button>
+          <p className=' w-[60%] text-[20px] text-white font-[400] leading-[32px]'>Invest securely and confidently on the go. Up to 35% returns, 6-12 month duration.</p>
+          <div className=' flex gap-4'>
+            <ButtonCard
+              icon={<FaApple color='#fff' size={20} />}
+              title="Get on iPhone"
+              textColor='#fff'
+              bgColor='#122231'
             />
-            <ButtonCard 
-            icon=''
-            title="Get on Andriod"
-            textColor='#fff'
-            bgColor='#122231'
+            <ButtonCard
+              icon={<BiLogoPlayStore  size={20}/>}
+              title="Get on Andriod"
+              textColor='#fff'
+              bgColor='#122231'
             />
           </div>
         </div>
-        <div></div>
+        <div className=' w-[50%] flex justify-end z-10'>
+          <img src={InvestHero} alt="" className=' w-[88%]'/>
+        </div>
       </div>
+        <div className=' absolute w-[400px] right-0 bottom-0'>
+          <img src={investBg} alt="" />
+        </div>
     </div>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+        <h1>dddd</h1>
+    </>
   )
 }
 
--- src/reuseable/ButtonCard.jsx
+++ src/reuseable/ButtonCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonCard = ({ title, bgColor, textColor, border, icon }) => {
   return (
-    <button className=' px-4 py-3 rounded-[10px] text-[15px] flex gap-2 items-center font-[500]
+    <button className=' px-4 py-3.5 rounded-[10px] text-[15px] flex gap-2 items-center font-[500]
                         max-sLaptop:px-3 max-sLaptop:py-2 max-sLaptop:text-[16px]
                         max-tablet:w-[100%] justify-center'
       style={{
