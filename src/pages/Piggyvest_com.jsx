7f88a40bc49f654e8589293d87f53da08b0ef69a
untitled <ahmedsuleiman940@gmail.com>
2025-06-30 19:59:41 +0100

    done the button routing

--- src/pages/Piggyvest_com.jsx
+++ src/pages/Piggyvest_com.jsx
@@ -1,49 +1,50 @@
 import React from 'react'
-import { BiLogoPlayStore } from "react-icons/bi";
-import { FaApple } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import PiggyWhite from "../assets/piggyWhite.png"
 import OnBoard_invest from "../assets/onboard_invest.svg";
 import OnBoard_target from "../assets/onboard_target.svg";
+import WhiteLogo from "../assets/white-logo.svg";
 
 const Piggyvest_com = () => {
   return (
     <div className=' text-center bg-[#14B25A] h-screen flex flex-col justify-start items-center py-18'>
-      <div className=' w-[440px] bg-amber-300 flex flex-col items-center gap-9'>
+      <div className=' w-[440px] flex flex-col items-center gap-9'>
 
 
-      <div>
-        <img src={PiggyWhite} alt="" />
-      </div>
+        <div>
+          <img src={WhiteLogo} alt="" />
+        </div>
 
-      <div className=' bg-red-500'>
-        <img src={OnBoard_target} alt="" className=' w-full' />
-      </div>
+        <div className=''>
+          <img src={OnBoard_target} alt="" className=' w-full' />
+        </div>
 
-      <div>
-        <h2>Setup multiple savings goals and reach them</h2>
-      </div>
+        <div>
+          <h2>Setup multiple savings goals and reach them</h2>
+        </div>
 
-      <p>. . . .</p>
+        <p>. . . .</p>
 
 
 
-<div className='flex flex-col gap-3 w-full'>
-
-        <button
-          className=' px-4 py-3.5 rounded-[10px] text-[17px] flex gap-2 items-center font-[500] w-full justify-center text-[#021737] bg-[#fff] 
+        <div className='flex flex-col gap-3 w-full'>
+          <Link to='/create-account'>
+            <button
+              className=' px-4 py-3.5 rounded-[10px] text-[17px] flex gap-2 items-center font-[500] w-full justify-center text-[#021737] bg-[#fff] 
                     '>
-          
-          Create an account
-        </button>
 
-
-        <button
-          className=' px-4 py-3.5 rounded-[10px] text-[17px] flex gap-2 items-center font-[500] w-full justify-center text-white bg-[#021737]
+              Create an account
+            </button>
+          </Link>
+          <Link to='/sign-in'>
+            <button
+              className=' px-4 py-3.5 rounded-[10px] text-[17px] flex gap-2 items-center font-[500] w-full justify-center text-white bg-[#021737]
                   '>
-          
-          Login
-        </button>
-</div>
+
+              Login
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   )
