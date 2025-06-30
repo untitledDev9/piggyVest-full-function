42af84f1850e6974729cbc60741c77170c4dd8a9
untitled <ahmedsuleiman940@gmail.com>
2025-06-30 12:34:58 +0100

    still working on invest

--- src/pages/invest/InvestHero.jsx
+++ src/pages/invest/InvestHero.jsx
@@ -8,6 +8,7 @@ const InvestHero = () => {
   return (
     <div className=' relative  max-w-[1280px] px-16 mx-auto flex pt-28 justify-between items-center
     max-tablet:flex-col max-tablet:bg-amber-400 max-tablet:w-[60%] max-tablet:text-center
+    max-tablet:px-5
     '>
       <div className=' flex flex-col gap-5'>
         <h1 className=' text-[48px] text-white font-[700]'>Invest on the go</h1>
@@ -15,10 +16,10 @@ const InvestHero = () => {
               max-tablet:mx-auto
               '> Up to 35% returns</button>
         <p className=' w-[60%] text-[20px] text-white font-[400] leading-[32px]
-              max-tablet:w-full
+              max-tablet:w-[80%] max-tablet:mx-auto
               '>Invest securely and confidently on the go. Up to 35% returns, 6-12 month duration.</p>
         <div className=' flex gap-4
-              
+              max-tablet:mx-auto max-tablet:my-8
               '>
           {/* <ButtonCard
                   icon={<FaApple color='#fff' size={20} />}
@@ -29,7 +30,8 @@ const InvestHero = () => {
                 /> */}
           <button className=' px-4 py-3.5 rounded-[10px] text-[15px] flex gap-2 items-center font-[500]
                         max-sLaptop:px-3 max-sLaptop:py-2 max-sLaptop:text-[16px]
-                        max-tablet:w-[100%] justify-center text-white bg-[#122231]
+                        justify-center text-white bg-[#122231]
+                        max-tablet:w-fit max-tablet:px-4  max-tablet:py-4
                         '>
             <FaApple color='#fff' size={20} />
             Get in iPhone
@@ -38,7 +40,8 @@ const InvestHero = () => {
 
           <button className=' px-4 py-3.5 rounded-[10px] text-[15px] flex gap-2 items-center font-[500]
                         max-sLaptop:px-3 max-sLaptop:py-2 max-sLaptop:text-[16px]
-                        max-tablet:w-[100%] justify-center text-white bg-[#122231]
+                        max-tablet:w-fit justify-center text-white bg-[#122231]
+                        max-tablet:px-4  max-tablet:py-4
                         '>
             <BiLogoPlayStore size={20} />
             Get on Andriod
@@ -54,7 +57,9 @@ const InvestHero = () => {
           /> */}
         </div>
       </div>
-      <div className=' w-[50%] flex justify-end z-10'>
+      <div className=' w-[50%] flex justify-end z-10
+      max-tablet:w-full max-tablet:justify-center
+      '>
         <img src={InvestHeroImage} alt="" className=' w-[88%]' />
       </div>
     </div>
