c37f0ab7eb33578f8e1c8cfb05917974c6383ec1
untitled <ahmedsuleiman940@gmail.com>
2025-07-05 19:29:43 +0100

    newsletter done

--- src/pages/resource/Newsletter.jsx
+++ src/pages/resource/Newsletter.jsx
@@ -10,6 +10,9 @@ import { FaArrowDownLong } from "react-icons/fa6";
 import resourceHand from "./image/imgi_36_phone-hand.997efe89163d536f444840aba9c30f53.png"
 import { IoLogoApple } from "react-icons/io";
 import { FaGooglePlay } from "react-icons/fa";
+import One from "./image/imgi_15_7ce5d757_c28c_4b2b_ae02_51fc8869816b_d5f9c522b7.jpg";
+import Two from "./image/imgi_16_a3b2c1ea_93d8_6a1d_bb8e_066f59785881_2a7ce1111c.png";
+import Three from "./image/imgi_17_8dc2248b_6d55_e0b8_55bf_dd21d7034223_3e1a2d0c82.png";
 
 const Newsletter = () => {
   return (
@@ -46,9 +49,73 @@ const Newsletter = () => {
           <p className='font-semibold text-xl'>Scroll down</p>
         </div>
       </div>
-
+<div className=' bg-white'>
+      <div className=' max-w-[1240px] py-18 mb-38 mx-auto px-16 max-tablet:mb-20'>
+          <div>
+            <div>
+              <h2 className=' text-2xl text-blue-500'>The Archive</h2>
+              <h2 className=' text-6xl max-tablet:text-[35px]'>Read Past</h2>
+            </div>
+            <div><input type="text" placeholder='Search for an issue' className=' py-3 pl-4 pr-38 rounded-xl mb-10 bg-[#F4F5F6]'/></div>
+          </div>
+          <div className=' flex flex-col border border-gray-200 rounded-2xl'>
+            <div className=' flex justify-between items-center px-7 py-5'>
+              <p>Issue</p>
+              <p>Date Released</p>
+            </div>
+            <div className=' flex bg-[#F6FAFF] justify-between px-7 py-5 items-center'>
+              <div className=' w-[80%] flex gap-3 items-center'>
+                <img src={One} alt=""  className="rounded-2xl w-[40px] h-[40px] object-cover"  />
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>Issue #03</p>
+                <p className=' font-semibold text-gray-700
+                max-tablet:text-[11px]
+                '>- We planned our dream wedding with 2 million</p>
+              </div>
+              <div>
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>28 Apr, 2023</p>
+              </div>
+            </div>
+            <div className=' flex justify-between px-7 py-5 items-center'>
+              <div className=' w-[80%] flex gap-3 items-center'>
+                <img src={Two} alt=""  className="rounded-2xl w-[40px] h-[40px] object-cover"  />
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>Issue #02</p>
+                <p className=' font-semibold text-gray-700
+                max-tablet:text-[11px]
+                '>- I have a monthly budget for spoiling my boyfriend</p>
+              </div>
+              <div>
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>31 Mar, 2023</p>
+              </div>
+            </div>
+            <div className=' flex bg-[#F6FAFF] justify-between px-7 py-5 items-center'>
+              <div className=' w-[80%] flex gap-3 items-center'>
+                <img src={Three} alt="" className="rounded-2xl w-[40px] h-[40px] object-cover  " />
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>Issue #01</p>
+                <p className=' font-semibold text-gray-700
+                max-tablet:text-[11px]
+                '>- I love my job, but I’m always stressed</p>
+              </div>
+              <div>
+                <p className=' text-gray-500
+                max-tablet:text-[9px]
+                '>24 Feb, 2023</p>
+              </div>
+            </div>
+          </div>
+      </div>
+</div>
       <div className=' rounded-3xl px-10 flex items-center relative max-w-[1240px] bg-[#1843D0] h-[550px] mx-auto
-        max-tablet:flex-col max-tablet:py-7
+        max-tablet:flex-col max-tablet:py-7 max-mobile:mb-28
         '>
         <div className=' flex flex-col gap-16
             max-tablet:items-center max-tablet:gap-6
@@ -66,6 +133,7 @@ const Newsletter = () => {
           <img src={resourceHand} alt="" />
         </div>
       </div>
+
       <Company />
       <Footer />
     </div>
